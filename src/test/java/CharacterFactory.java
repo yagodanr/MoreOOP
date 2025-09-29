bde2757e00fe4db6ba472c9f6cb4da5ada93e267
yagodanr <yagodanr@gmail.com>
2025-09-29 18:42:56 +0300

    CharacterFactory: Reflections package

--- src/test/java/CharacterFactory.java
+++ src/test/java/CharacterFactory.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
+
+import org.reflections.Reflections;
 
 import characters.Character;
 import characters.Elf;
@@ -13,12 +16,13 @@ import lombok.SneakyThrows;
 public class CharacterFactory {
     private static Random rand = new Random();
     private static List<Class<? extends Character>> classes = new ArrayList<>();
+    private static Reflections reflections = new Reflections();
+    private static Set<Class<? extends Character>> allClasses = reflections.getSubTypesOf(Character.class);
+
     public CharacterFactory() {
-        classes.add(Hobbit.class);
-        classes.add(Elf.class);
-        classes.add(King.class);
-        classes.add(Knight.class);
+        classes.addAll(allClasses);
     }
+
     @SneakyThrows
     Character createCharacter() {
         return classes.get(rand.nextInt(classes.size())).getConstructor().newInstance();
